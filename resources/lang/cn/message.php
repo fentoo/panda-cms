439fe52a014f91db04bc01558569d4fb2822764f
fentoo <pengqinghui@gmail.com>
2016-11-15 15:50:29 +0800

    add 404 not found template

--- resources/lang/cn/message.php
+++ resources/lang/cn/message.php
@@ -3,6 +3,8 @@ return [
     /**
      * Universal message
      */
+    'page_not_found' => '页面不存在',
+
     'maintain' => '系统升级维护中',
     'maintain_start_at' => '维护开始时间：:time',
     'maintain_retry_after' => '请在 :seconds 秒后重试',
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@lang('message.page_not_found')</title>
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                color: #B0BEC5;
+                display: table;
+                font-weight: 100;
+                font-family: 'Microsoft YaHei', sans-serif;
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 4em;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">404 - @lang('message.page_not_found')</div>
+            </div>
+        </div>
+    </body>
+</html>
